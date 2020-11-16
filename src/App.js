bc5839f527ebbbc7ac2bfffbf8a00900444eb148
erick uriel <uriel.noriegam@gmail.com>
2020-11-16 12:36:19 -0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+import { ChakraProvider } from "@chakra-ui/react"
+import { IconButton } from "@chakra-ui/react"
+import { SearchIcon } from '@chakra-ui/icons'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <div>
+        <IconButton aria-label="Search database" icon={<SearchIcon />} />
+      </div>
+    </ChakraProvider>
   );
 }
 
